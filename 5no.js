86ad2c07b738ba83acad9ba7d60181bff852c292
Auntor Acharja <auntoracharja@Auntors-MacBook-Air.local>
2022-10-02 17:46:52 +0600

    Solve No. 5 Prblm

---
+++ 5no.js
@@ -0,0 +1,17 @@
+// How to Convert String to Integer to String
+
+var string ="123"
+var int = 123
+
+// Convert string to int
+let aftetConvert_Int= parseInt(string)
+console.log(aftetConvert_Int)
+console.log(typeof(aftetConvert_Int))
+
+
+// Convert int to string
+let afterConvert_String = int.toString()
+console.log(afterConvert_String)
+console.log(typeof(afterConvert_String))
+
+
